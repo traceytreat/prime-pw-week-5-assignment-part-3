3c06fbde44bfe24f04c349561eb79d6b666eec89
Tracey Treat <tracey_treat@yahoo.com>
2022-10-23 19:49:03 -0500

    added addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,21 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+function addToCollection (title, artist, yearPublished){
+    const music = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    collection.push(music);
+    return music;
+}
+
+console.log(addToCollection('Album1', 'Artist1', 2001));
+console.log(addToCollection('Album2', 'Artist2', 2002));
+console.log(addToCollection('Album3', 'Artist3', 2003));
+console.log(addToCollection('Album4', 'Artist4', 2004));
+console.log(addToCollection('Album5', 'Artist5', 2005));
+console.log(addToCollection('Album6', 'Artist6', 2006));
+
+console.log(collection);
